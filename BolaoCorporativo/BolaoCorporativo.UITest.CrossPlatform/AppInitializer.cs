7987b1a93ea4101342d731a47489e1c55635cecc
Ewerton Cerqueira Rodrigues <ewertonrodrigues@yahoo.com.br>
2017-07-28 12:40:59 -0300

    Subindo nova versão dos testes.

---
+++ BolaoCorporativo/BolaoCorporativo.UITest.CrossPlatform/AppInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace BolaoCorporativo.UITest.CrossPlatform
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .ApkFile("../../../BolaoCorporativo.Android/bin/Release/br.com.squadra.bolaocorporativo.android.apk")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                //.AppBundle("/path/to/iosapp.app")
+                .StartApp();
+        }
+    }
+}
+
--- BolaoCorporativo/BolaoCorporativo/MainPage.xaml.cs
+++ BolaoCorporativo/BolaoCorporativo/MainPage.xaml.cs
@@ -12,8 +12,6 @@ namespace BolaoCorporativo
         public MainPage()
         {
             InitializeComponent();
-
-            throw new NotImplementedException();
         }
     }
 }
