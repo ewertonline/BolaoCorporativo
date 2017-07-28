bfc8cebaa265a8a848ac4884c11cfd1ec04a9dc9
Ewerton Cerqueira Rodrigues <ewertonrodrigues@yahoo.com.br>
2017-07-27 22:55:15 -0300

    Crash and Analytics

--- BolaoCorporativo/BolaoCorporativo/App.xaml.cs
+++ BolaoCorporativo/BolaoCorporativo/App.xaml.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 using Xamarin.Forms;
 
@@ -19,6 +22,11 @@ namespace BolaoCorporativo
         protected override void OnStart()
         {
             // Handle when your app starts
+
+            MobileCenter.Start(
+                   "android=2b5ee3db-07c3-406c-aa34-9c311347ab4a;" +
+                   "ios=3fa79e98-d61a-452d-a950-a77cdf688745;",
+                   typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
